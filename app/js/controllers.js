2c97bed5d6d5806efed49e05e92bd441ade7e16a
Zachary Kearns <zach_kearns89@live.com>
2017-03-21 09:10:16 -0400

    first commit

---
+++ app/js/controllers.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('myApp.controllers', []).
+  controller('FinanceController', function($scope) {
+  $scope.salary = 0;
+  $scope.percentage = 0;
+  $scope.result = function() {
+    return $scope.salary * $scope.percentage * 0.01;
+  };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ORIGINAL CODE
+/* Controllers */
+
+// angular.module('myApp.controllers', []).
+//   controller('MyCtrl1', [function() {
+
+//   }])
+//   .controller('MyCtrl2', [function() {
+
+//   }]);
